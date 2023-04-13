cb29c4f69b3fc347af4c01cd1be860d7e0c578b4
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-13 13:04:05 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import SearchList from './SearchList'
+import searchResults from './searchResults'
+
+function App() {
+
+     
+
+     return (
+          <div className='container'>
+               <div className="header">
+                    <input type="text" />
+               </div>
+               <div className="body">
+
+               </div>
+          </div>
+     )
+}
+
+export default App
---
+++ src/SearchList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchList() {
+     return (
+          <div>
+               <li></li>
+          </div>
+     )
+}
+
+export default SearchList
